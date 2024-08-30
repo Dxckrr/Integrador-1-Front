c93733b55b097065d8db60dce6e0757fdd854a8c
Duck <juandavidp1127@gmail.com>
2024-08-30 12:21:28 -0500

    feat routes structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,52 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';  // Importando librerias de react-router-dom para el manejo de wards ('/')
-import Dashboard from './components/views/Dashboard';
+import { BrowserRouter, Route, Routes } from "react-router-dom"; // Importando librerias de react-router-dom para el manejo de wards ('/')
+import Dashboard from "./components/pages/Dashboard";
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
+      <Routes>
+        {/* MAIN ROUTE '/' */}
+        <Route index element={<Dashboard />} />
+        {/* USUARIO */}
+        <Route path="/" element={<Dashboard />} />
+        {/* 
+        <Route path="/" element={<AboutUs />} />
+        <Route path="/" element={<Services />} /> 
+        <Route path="/" element={<Appointments />} />         
+        */}
 
-      {/* MAIN ROUTE '/' */}
-      <Route index element={<Dashboard />} />
-      {/* USUARIO */}
-      <Route path='/' element={<Dashboard />} />
-      
-      {/* OPERADOR */}
-      {/* <Route path='/test' element={<Dashboard />} /> */}
+        {/* OPERADOR */}
+
+        {/* AQUI IRA LA RUTA PROTEGIDA DE OPERADOR
+        <Route path='/operator' element={<Dashboard />}>     
+          <Route path='/sth/' element={<Dashboard />}>
+        </Route>
+
+         */}
+
+        {/* MEDICO */}
+
+        {/* AQUI IRA LA RUTA PROTEGIDA DE MEDICO
+        <Route path='/medic' element={<Dashboard />}>     
+          <Route path='/sth/' element={<Dashboard />}>
+        </Route>
+         */}
+
+        {/* ADMINISTRADOR */}
+
+        {/* AQUI IRA LA RUTA PROTEGIDA DE ADMIN
+        <Route path='/admin' element={<Dashboard />}>     
+          <Route path='/sth/' element={<Dashboard />}>
+        </Route>
+         */}
 
 
-    </Routes>
-  </BrowserRouter >
+        {/* RUTAS NO EXISTENTES */}
+        {/* <Route path="*" element={<NotFound />} /> */}
+
+        {/* HealthCheck */}
+        {/* <Route path="/health" element={<HealthCheck />} /> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 export default App;
