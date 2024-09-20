5f72c77bef5594f33f123603ce7cd10990f70e28
JCarvajalr <juandacr25@gmail.com>
2024-09-19 23:27:49 -0500

    feat sideBarAmin component

---
+++ src/components/pages/admin/AdminFinances.tsx
@@ -0,0 +1,16 @@
+
+/**
+Contains the page to manage the finances of the ips
+@returns {Component} AdminStats
+*/
+const AdminFinances = () => {
+    return (
+        <>
+            <main className="min-h-screen w-full flex justify-center bg-gradient-to-b from-white to-[#EFF0F1]">
+            
+            </main>
+        </>
+
+    )
+}
+export default AdminFinances
---
+++ src/components/pages/admin/AdminStats.tsx
@@ -0,0 +1,16 @@
+
+/**
+Contains the page to view the stats
+@returns {Component} AdminStats
+*/
+const AdminStats = () => {
+    return (
+        <>
+            <main className="min-h-screen w-full flex justify-center bg-gradient-to-b from-white to-[#EFF0F1]">
+            
+            </main>
+        </>
+
+    )
+}
+export default AdminStats
